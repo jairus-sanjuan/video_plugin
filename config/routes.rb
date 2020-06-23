d6e4ce6417b359780a5d69832821ad065ee722b0
Jairus San Juan <san.jairus@gmail.com>
2020-06-23 14:50:10 +0800

    added initial state of users & videos controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'videos/index'
+  get 'videos/show'
+  get 'videos/create'
+  get 'users/index'
+  get 'users/show'
+  get 'users/create'
   root 'pages#index'
   get '*path', to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
