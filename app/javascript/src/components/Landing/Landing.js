87502721edb0600276b8bb3504f9d891f66b7fc1
Jairus San Juan <san.jairus@gmail.com>
2020-06-23 10:32:23 +0800

    added root page, react as templating engine

---
+++ app/javascript/src/components/Landing/Landing.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>This is index page.</h1>
+    </div>
+  )
+}
+export default Landing
---
+++ app/javascript/src/components/app.js
@@ -0,0 +1,29 @@
+import React, { Fragment } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Landing from './Landing/Landing'
+
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <Switch>
+          <Route path="/" component={Landing} />
+        </Switch>
+      </Fragment>
+    </Router>
+  )
+}
+
+// const app = () => {
+//   return (
+//     <Router>
+//       <Fragment>
+//         <Switch>
+//           <Route path="/" component={Landing} />
+//         </Switch>
+//       </Fragment>
+//     </Router>
+//   )
+// }
+
+export default App
