ec3cfd10f5b9eb4e7e80eeb447e206984e4cb48b
Jairus San Juan <san.jairus@gmail.com>
2020-06-25 17:46:09 +0800

    removed landing page gibberish

--- app/javascript/src/components/Landing/Landing.js
+++ app/javascript/src/components/Landing/Landing.js
@@ -2,20 +2,6 @@ import React, { useEffect } from 'react'
 import axios from 'axios'
 
 const Landing = () => {
-  // useEffect(() => {
-  //   axios
-  //     .get('http://localhost:3000/session', {
-  //       withCredentials: true,
-  //     })
-  //     .then((resp) => {
-  //       console.log("Response : ",resp)
-  //     })
-  // }, [])
-
-  return (
-    <div className="Landing">
-      <h1>Hello this is landing page.</h1>
-    </div>
-  )
+  return <div className="Landing"></div>
 }
 export default Landing
